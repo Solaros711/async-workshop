7433ee3865182c750b2c469510d3d9def0ad36e4
Solaros711 <spurdy885@gmail.com>
2020-07-31 21:25:18 -0700

    Finished async1

--- src/async.js
+++ src/async.js
@@ -1,7 +1,27 @@
 // Async Utilities
 
 function parallel (tasks, callback) {
+  let array = new Array(tasks.length)
+  var cerr = null
+  let count = 0
 
+  for(let i = 0; i < tasks.length; i++){
+    tasks[i](function(err, result){
+      if(err != null){
+        callback(err)
+        return
+      }
+  
+      array[i] = result
+  
+      count++
+  
+      if(count === tasks.length){
+        callback(cerr, array)
+        return
+      }
+    })
+  }
 }
 
 // Try to define map using parallel
